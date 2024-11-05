cb78f5bc97b400cca64ede4fef13ef096601fa63
Alex Junior <alexjr2oficial@gmail.com>
2024-11-05 18:52:18 -0300

    trabalhando nos testes unitarios

---
+++ APICatalogo_xUnitTest/UnitTests/ProdutosUnitTestController.cs
@@ -0,0 +1,38 @@
+﻿using APICatalogo.Context;
+using APICatalogo.DTOs;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Repositories;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo_xUnitTest.UnitTests
+{
+    public class ProdutosUnitTestController
+    {
+        public IUnitOfWork repository;
+        public IMapper mapper;
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+        public static string connectionString =
+            "\"Data Source=.\\\\mydb.db;\"";
+
+        static ProdutosUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite()
+                .Options;
+        }
+
+        public ProdutosUnitTestController()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ProdutoDTOMappingProfile());
+            });
+
+            mapper = config.CreateMapper();
+            var context = new AppDbContext(dbContextOptions);
+            repository = new UnitOfWork(context);
+        }
+    }
+}
